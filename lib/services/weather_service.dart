359f28f3b430436aa2e53288a89c89679a9a78ae
tasneemkhaled <khaledtasneem402@gmail.com>
2025-03-03 22:51:55 +0200

    dio exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,17 +6,27 @@ class WeatherService {
   final String baseUrl = 'http://api.weatherapi.com/v1';
   final String apiKey = '11a4603114524911acd154955241608';
   WeatherService({required this.dio});
-  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
     // try {
-    Response response = await dio.get(
-        '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1&aqi=no&alerts=no');
-    if (response.statusCode == 200) {
+    try {
+      Response response = await dio.get(
+          '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1&aqi=no&alerts=no');
       WeatherModel weatherModel = WeatherModel.fromJson(response.data);
       return weatherModel;
-    } else {
-      final String errorMesaage = response.data['error']['message'];
-      throw Exception(errorMesaage);
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data['error']['message'] ??
+          'there was an error pls try later';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('try later');
     }
+
+    // if (response.statusCode == 200) {
+
+    // } else {
+    //   final String errorMesaage = response.data['error']['message'];
+    //   throw Exception(errorMesaage);
+    // }
     // } on Exception catch (e) {
     //   return null;
   }
