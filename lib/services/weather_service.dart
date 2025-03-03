08574ce8186c0fda2c03e9a656e459a76b5976dd
tasneemkhaled <khaledtasneem402@gmail.com>
2025-03-03 14:02:05 +0200

    handeling exceptions

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,11 +4,16 @@ import 'package:weather_app/models/weather_model.dart';
 class WeatherService {
   final Dio dio;
   final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String apiKey = '11a4603114524911acd154955241608';
   WeatherService({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-        '$baseUrl/forecast.json?key=11a4603114524911acd154955241608&q=$cityName&days=1&aqi=no&alerts=no');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+          '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1&aqi=no&alerts=no');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on Exception catch (e) {
+      return null;
+    }
   }
 }
