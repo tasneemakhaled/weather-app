d85d2c7f73cdacca561ce4e4239a982baa07aae6
tasneemkhaled <khaledtasneem402@gmail.com>
2025-03-06 07:45:49 +0200

    integrate cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/cubit/get_weather_cubit.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/no_weather_view.dart';
 import 'package:weather_app/widgets/weather_info.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +28,17 @@ class HomeView extends StatelessWidget {
               icon: Icon(Icons.search)),
         ],
       ),
-      // body: weatherModel != null ? WeatherInfoBody() : NoWeatherBody(),
+      body: BlocBuilder<GetWeatherCubit, GetWeatherState>(
+        builder: (context, state) {
+          if (state is NoWeatherState) {
+            return NoWeatherBody();
+          } else if (state is WeatherLoadedState) {
+            return WeatherInfoBody();
+          } else {
+            return Text('There was an error pls try later');
+          }
+        },
+      ),
     );
   }
 }
